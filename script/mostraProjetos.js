d210d9269468715e7edb0f5e7d8b5651d6550348
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-07-24 15:34:53 -0300

    nome projetos

--- script/mostraProjetos.js
+++ script/mostraProjetos.js
@@ -3,6 +3,7 @@ import { conectaApi } from "./conectaApi.js";
 const lista = document.querySelector("[data-lista]");
 
 export default function constroiCard (name, html_url) {
+    
     const projetos = document.createElement("li");
     projetos.className = `texto_texto_curriculo_li`;
     projetos.innerHTML = ` 
@@ -17,7 +18,7 @@ async function listaProjetos() {
         const listaApi = await conectaApi.listaProjetos();
         if (listaApi.length > 0) {
             listaApi.forEach(elemento => {
-                lista.appendChild(constroiCard(elemento.name, elemento.html_url));
+                lista.appendChild(constroiCard(elemento.name.replace(/-/g, ' '), elemento.html_url));
             });
         } else {
             products.innerHTML = `<h2 class="mensagem__titulo">Nenhum projeto foi adicionado!</h2>`;
