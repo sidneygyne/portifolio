9e857847c6bf01ee75c4b168df9d2be56a78d126
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-07-30 13:27:29 -0300

    atualização curriculo

--- script/mostraProjetos.js
+++ script/mostraProjetos.js
@@ -5,9 +5,9 @@ const lista = document.querySelector("[data-lista]");
 export default function constroiCard (name, html_url) {
     
     const projetos = document.createElement("li");
-    projetos.className = `texto_texto_curriculo_li`;
+    projetos.className = `texto_texto_curriculo_li ${name}`;
     projetos.innerHTML = ` 
-         <a class="texto_texto_curriculo_links" href="${html_url}" target="_blank"><strong>${name}</strong></a> 
+         <a class="texto_texto_curriculo_links" href="${html_url}" target="_blank"><strong>${name.replace(/-/g, ' ')}</strong></a> 
         `
     return projetos;
 }
@@ -18,7 +18,7 @@ async function listaProjetos() {
         const listaApi = await conectaApi.listaProjetos();
         if (listaApi.length > 0) {
             listaApi.forEach(elemento => {
-                lista.appendChild(constroiCard(elemento.name.replace(/-/g, ' '), elemento.html_url));
+                lista.appendChild(constroiCard(elemento.name, elemento.html_url));
             });
         } else {
             products.innerHTML = `<h2 class="mensagem__titulo">Nenhum projeto foi adicionado!</h2>`;
